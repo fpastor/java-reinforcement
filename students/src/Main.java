0829652bfc4e62e57a2129fa856303c6eb8f1fdb
Fernando Pastor <fpastor@icloud.com>
2024-08-06 15:30:56 +0200

    students

---
+++ students/src/Main.java
@@ -0,0 +1,12 @@
+//
+//  Main.java
+//  students
+//
+//  Created by Fernando Pastor on 06/08/2024
+//
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
