b9fe5c3dd154ebce2704451f2108f9645db140a4
Fernando Pastor <fpastor@icloud.com>
2024-08-06 15:28:34 +0200

    ex8 customException

---
+++ ex8-customException/src/Main.java
@@ -0,0 +1,12 @@
+//
+//  Main.java
+//  ex8-customException
+//
+//  Created by Fernando Pastor on 06/08/2024
+//
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
